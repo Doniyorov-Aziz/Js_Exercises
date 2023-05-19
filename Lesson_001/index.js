4fd7c72212293b755dba62d2ce8ab5683241647e
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-05-19 18:15:22 +0500

    exercises

--- Lesson_001/index.js
+++ Lesson_001/index.js
@@ -1,8 +1,35 @@
 // #1  berilgan stringni teskarisiga chiqarish
 
-let str = "webbrain";
-let res = str.split("").reverse().join("");
-console.log(res);
+// let str = "webbrain";
+// let res = str.split("").reverse().join("");
+// console.log(res);
 
-// #2 
+// #2
+// bizga berilgan so'zni joyini almashtirib qo'yish
 
+// let str = "john McClane"
+// console.log(str.split(" ").reverse().toString().replace(","," "));
+
+// #3
+
+// function getName() {
+//   let x = "";
+//   let y = "";
+//   if (x > y) console.log("win");
+//   else if (x < y) console.log("loss");
+//   else if ((x = y)) console.log("tie");
+//   else console.log("undefined");
+// }
+// getName();
+
+//
+
+// name = "webbrain"
+
+// let obj = {
+// name,
+// }   
+// console.log(obj.name);
+
+// let num = "webbbbbbrain"
+// console.log(num.replace("b"," "));
